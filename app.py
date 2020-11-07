c3831e923c1210dc9cacd2c24154fa8b8f27226b
Jiyong <jiyong1972@gmail.com>
2020-11-07 14:37:50 +0900

    fixed some bugs...

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request, jsonify
 from flask_restful import Api
 from flask_jwt import JWT
@@ -14,7 +16,9 @@ app.config['DEBUG'] = True
 app.secret_key = 'jiyong'
 app.config['JWT_AUTH_URL_RULE'] = '/login'
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
 
